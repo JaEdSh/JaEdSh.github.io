b4469db049b260864f3a8b57e7bea66022901599
JaEdSh <122374849+JaEdSh@users.noreply.github.com>
2024-08-21 12:22:38 -0500

    Add files via upload

---
+++ ExtendedDatepicker.js
@@ -0,0 +1,62 @@
+import { css, html, LitElement, styleMap, until } from 'https://cdn.jsdelivr.net/gh/lit/dist@2/all/lit-all.min.js';
+
+export class ExtendedDatePicker extends LitElement {
+
+
+    static get properties() {
+        return {
+            value: { type: String }
+          };
+    }
+
+    static getMetaConfig() {
+        // plugin contract information
+        return {
+            controlName: 'Start Workflow',
+            fallbackDisableSubmit: false,
+            description: 'Connects to the Nintex API to start a workflow',
+            iconUrl: "one-line-text",
+            groupName: 'Custom Controls',
+            version: '1.4',
+            //This holds all the parameters that are entered into the control.
+            properties: {
+                default: {
+                    type: 'string',
+                    title: 'Default Value'
+                },
+                value: {
+                    type: 'string',
+                    title: 'Value',
+                    isValueField: true
+                }
+            },
+            //Triggers an event that the Nintex form can handle
+            events: ["ntx-value-change"]
+
+        };
+    }
+
+    onChange(inputE) {
+        if (this.startRun != null) {
+            this.value = inputE;
+            console.log(this.value);
+            const args = {
+                bubbles: true,
+                cancelable: false,
+                composed: true,
+                detail: inputE,
+            };
+            const event = new CustomEvent('ntx-value-change', args);
+            this.dispatchEvent(event);
+        }
+    }
+
+    // Render the UI as a function of component state
+    render() {
+        return html`<mwc-textfield id="datefield" type="date" onChange="onChange(${this.value})">${this.value}</mwc-textfield>`
+    }
+}
+
+// registering the web component.
+const elementName = 'extended-datepicker';
+customElements.define(elementName, ExtendedDatePicker);
