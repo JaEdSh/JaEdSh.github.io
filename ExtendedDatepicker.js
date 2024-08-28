6d4e789383b8755361ca7aa0f4faf4a75c4b7829
JaEdSh <122374849+JaEdSh@users.noreply.github.com>
2024-08-28 11:41:21 -0500

    Add files via upload

--- ExtendedDatepicker.js
+++ ExtendedDatepicker.js
@@ -18,7 +18,7 @@ export class ExtendedDatePicker extends LitElement {
                     type: 'string',
                     title: 'Default Value',
                     defaultValue: '08/27/2024'
-                },
+                }
             },
             //Standard nwc control properties
             standardProperties: {
@@ -30,28 +30,13 @@ export class ExtendedDatePicker extends LitElement {
         }
     }
 
-    /*onChange(inputE) {
-        if (this.startRun != null) {
-            this.value = inputE;
-            console.log(this.value);
-            const args = {
-                bubbles: true,
-                cancelable: false,
-                composed: true,
-                detail: inputE,
-            };
-            const event = new CustomEvent('ntx-value-change', args);
-            this.dispatchEvent(event);
-        }
-    }*/
-
     constructor() {
         super();
     }
 
     // Render the UI as a function of component state
     render() {
-        return html`<input id="datefield" class="nx-datetime-control" type="date" readonly="${this.readOnly}" value="${this.defaultValue}">${this.value}</input>`
+        return html`<input id="datefield" class="nx-datetime-control" type="date" readonly="${this.readOnly}" value="${this.defaultValue}"></input>`
     }
 }
 
